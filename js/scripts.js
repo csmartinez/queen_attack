17e07a412be34b9f2274fd0e4a79a56bb885f53d
Lee Jorgensen & Carli Martinez <student@epicodus.com>
2015-02-09 12:21:40 -0800

    initialized queen attack with java script

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var queenAttack = function(queen_position, attack_square) {
+    if ((queen_position[0] === attack_square[0] || queen_position[1] === attack_square[1])) {
+      return true;
+    } else if ((queen_position[0] === ))
+    return true;
+    }
+};
---
+++ spec/specs.js
@@ -0,0 +1,14 @@
+describe('queenAttack', function() {
+  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other', function() {
+    expect(queenAttack([1, 1], [2, 3])).to.equal(false);
+  });
+  it('is true if the x coordinates are matching', function() {
+    expect(queenAttack([2, 3], [2, 8])).to.equal(true);
+  });
+  it('is true if the y coordinates are matching', function() {
+    expect(queenAttack([2, 8], [4, 8])).to.equal(true);
+  });
+  it('is true if the queen can attack diagonally'), function() {
+    expect(queenAttack([2,2], [4,4])).to.equal(true);
+  });
+});
